331a1c104acc6ea10b7728fdc4c28021549b1321
Dennis Vidovic <dvidovic@meraki.com>
2022-11-10 09:44:58 -0800

    cmp updates

--- lua/plugin-configs/cmp-setup.lua
+++ lua/plugin-configs/cmp-setup.lua
@@ -13,9 +13,14 @@ local cmp_ap = safe_require('nvim-autopairs.completion.cmp')
 if not cmp_ap then return end
 
 cmp.event:on(
-  'confirm_done',
-  cmp_ap.on_confirm_done()
+    'confirm_done',
+    cmp_ap.on_confirm_done()
 )
+
+local function has_words_before()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
 
 local kind_icons = {
     Text = "",
@@ -64,7 +69,35 @@ cmp.setup {
         },
         -- Accept currently selected item. If none selected, `select` first item.
         -- Set `select` to `false` to only confirm explicitly selected items.
-        ["<CR>"] = cmp.mapping.confirm { select = true },
+        ["<CR>"] = cmp.mapping.confirm { select = false },
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expandable() then
+                luasnip.expand()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
     },
     formatting = {
         fields = { "kind", "abbr", "menu" },
@@ -98,4 +131,3 @@ cmp.setup {
         completion = cmp.config.window.bordered(),
     },
 }
-
