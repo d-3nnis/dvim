7b6713b14993bcaa8b447e7a0ff1da55f989dc1f
Dennis <dennis.vidovic@gmail.com>
2022-11-18 22:58:08 -0800

    Added copy command autocmd highlight

--- lua/post-plugins.lua
+++ lua/post-plugins.lua
@@ -1,5 +1,10 @@
 vim.opt.termguicolors = true
 
+local au = vim.api.nvim_create_autocmd
+local ag = vim.api.nvim_create_augroup
+
+au('TextYankPost', { group = ag('yank_highlight', {}), pattern = '*', callback = function() vim.highlight.on_yank { higroup='IncSearch', timeout=300 } end, })
+
 local onedark = safe_require('onedark')
 if onedark
 then
