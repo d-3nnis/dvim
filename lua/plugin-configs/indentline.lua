45b657251e06355f9bc3949e7db518efe10501f2
Dennis Vidovic <dvidovic@meraki.com>
2023-01-18 11:00:41 -0800

    Fixed tab listchars

--- lua/plugin-configs/indentline.lua
+++ lua/plugin-configs/indentline.lua
@@ -1,12 +1,12 @@
 local ts = safe_require('nvim-treesitter')
 if ts then
-    vim.opt.listchars = { eol = '﬋', tab = ' ', trail = '·' }
+    vim.opt.listchars = { eol = '﬋', tab = '--', trail = '·' }
 end
-
+		
 local il = safe_require('indent_blankline')
 if not il then return end
 
 il.setup {
     show_current_context = true,
-    show_current_context_start = false,
+    show_current_context_start = true,
 }
