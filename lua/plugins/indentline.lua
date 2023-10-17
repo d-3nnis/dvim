a42fb862c33976c9858b10f52ca7ae8849392c82
dvidovic <dennis.vidovic@meraki.net>
2023-10-17 14:49:15 -0700

    Fixed indents

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -1,26 +1,20 @@
 local config = {
-{
-    'lukas-reineke/indent-blankline.nvim',
-    config = function()
+    {
+        'lukas-reineke/indent-blankline.nvim',
+        main = "ibl",
+        opts = {},
+        config = function()
+            local ts = safe_require('nvim-treesitter')
+            if ts then
+                -- vim.opt.listchars = { eol = '﬋', tab = '--', trail = '·' }
+            end
 
+            local il = safe_require('ibl')
+            if not il then return end
 
-local ts = safe_require('nvim-treesitter')
-if ts then
-    vim.opt.listchars = { eol = '﬋', tab = '--', trail = '·' }
-end
-		
-local il = safe_require('indent_blankline')
-if not il then return end
-
-il.setup {
-    show_current_context = true,
-    show_current_context_start = true,
-}
-
-
+            il.setup {}
         end
     }
-
-
 }
+
 return config
