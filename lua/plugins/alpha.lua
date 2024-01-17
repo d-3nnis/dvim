117f9fc67ad37566e1833c3b5579a2f7796d005e
Dennis Vidovic <dvidovic@meraki.com>
2024-01-17 11:18:03 -0800

    Fixed spacing

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -18,9 +18,9 @@ local config = {
             dashboard.section.buttons.val = {
                 dashboard.button("f", " Find file", ":Telescope find_files <CR>"),
                 -- dashboard.button("a", "  Sessions list", ":lua safe_require('nvim-possession').list()<CR>"),
-                dashboard.button("e", "  Find project", ":Telescope projects <CR>"),
+                dashboard.button("e", " Find project", ":Telescope projects <CR>"),
                 dashboard.button("r", "󱫤 Recently used files", ":Telescope oldfiles <CR>"),
-                dashboard.button("v", "  Edit dvim", ":e ~/.config/nvim/init.lua <CR>"),
+                dashboard.button("v", " Edit dvim", ":e ~/.config/nvim/init.lua <CR>"),
                 dashboard.button("q", " Quit Neovim", ":qa<CR>"),
             }
 
