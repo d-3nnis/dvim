78a7c81ae803430524820d40d5378b6ccf0a294a
Dennis Vidovic <dvidovic@meraki.com>
2024-04-25 13:45:50 -0700

    Updated copilot chat prompts

--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -1,15 +1,37 @@
+local select = require('CopilotChat.select')
+
 local config = {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "canary",
+        dependencies = {
+            { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+            { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+        },
+        opts = {
+            -- See Configuration section for rest
+            -- default mappings
+            mappings = {
+                reset = {
+                    normal = '<C-r>',
+                    insert = '<C-r>'
+                },
+            },
+            prompts = {
+                Commit = {
+                    prompt =
+                    'Write commit message for the change. No specific format is required. Just call out the contents of the change. Make sure the title has maximum 50 characters and message is wrapped at 80 characters. Wrap the whole message in code block with language gitcommit.',
+                    selection = select.gitdiff,
+                },
+                CommitStaged = {
+                    prompt =
+                    'Write commit message for the change. No specific format is required. Just call out the contents of the change. Make sure the title has maximum 50 characters and message is wrapped at 80 characters. Wrap the whole message in code block with language gitcommit.',
+                    selection = function(source)
+                        return select.gitdiff(source, true)
+                    end,
+                },
+            },
+        },
     },
-    opts = {
-      -- See Configuration section for rest
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
 }
 return config
