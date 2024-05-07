c5b4264691167e83f3edd093e17cac9396e32f3a
Dennis Vidovic <dvidovic@meraki.com>
2024-05-07 11:45:48 -0700

    Added osc52 support to oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -14,7 +14,22 @@ local config = {
                 ["<BS>"] = "actions.parent",
                 ["gw"] = "actions.open_cwd",
                 ["."] = "actions.cd",
-                ["gy"] = "actions.copy_entry_path",
+                ["gy"] = {
+                    callback = function()
+                        local osc52 = safe_require("osc52")
+                        if not osc52 then
+                            require('oil.actions').copy_entry_path.callback()
+                            return
+                        end
+                        local entry = oil.get_cursor_entry()
+                        local dir = oil.get_current_dir()
+                        if not entry or not dir then
+                          return
+                        end
+                        local full_path = dir .. entry.name
+                        osc52.copy(full_path)
+                    end, desc = "osc52 copy full path", mode = "n"
+                },
                 ["gs"] = "actions.change_sort",
                 ["J"] = "actions.toggle_hidden",
             },
