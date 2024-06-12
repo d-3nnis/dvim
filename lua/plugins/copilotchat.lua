e06146671672cd60e004bdcec50e77358a0d4758
Dennis Vidovic <dvidovic@meraki.com>
2024-06-12 10:11:19 -0700

    Updated copilot chat prompts

--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -1,5 +1,3 @@
-local select = require('CopilotChat.select')
-
 local config = {
     {
         "CopilotC-Nvim/CopilotChat.nvim",
@@ -8,30 +6,33 @@ local config = {
             { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
             { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
         },
-        opts = {
-            -- See Configuration section for rest
-            -- default mappings
-            mappings = {
-                reset = {
-                    normal = '<C-r>',
-                    insert = '<C-r>'
+        config = function()
+            local copilotchat = safe_require('CopilotChat')
+            local select = safe_require('CopilotChat.select')
+            if not copilotchat then return end
+            copilotchat.setup {
+                mappings = {
+                    reset = {
+                        normal = '<C-r>',
+                        insert = '<C-r>'
+                    },
                 },
-            },
-            prompts = {
-                Commit = {
-                    prompt =
-                    'Write commit message for the change. No specific format is required. Just call out the contents of the change. Make sure the title has maximum 50 characters and message is wrapped at 80 characters. Wrap the whole message in code block with language gitcommit.',
-                    selection = select.gitdiff,
+                prompts = {
+                    Commit = {
+                        prompt =
+                        'Write commit message for the change. No specific format is required. Just call out the contents of the change. Make sure the title has maximum 50 characters and message is wrapped at 80 characters. Wrap the whole message in code block with language gitcommit.',
+                        selection = select.gitdiff,
+                    },
+                    CommitStaged = {
+                        prompt =
+                        'Write commit message for the change. No specific format is required. Just call out the contents of the change. Make sure the title has maximum 50 characters and message is wrapped at 80 characters. Wrap the whole message in code block with language gitcommit.',
+                        selection = function(source)
+                            return select.gitdiff(source, true)
+                        end,
+                    },
                 },
-                CommitStaged = {
-                    prompt =
-                    'Write commit message for the change. No specific format is required. Just call out the contents of the change. Make sure the title has maximum 50 characters and message is wrapped at 80 characters. Wrap the whole message in code block with language gitcommit.',
-                    selection = function(source)
-                        return select.gitdiff(source, true)
-                    end,
-                },
-            },
-        },
+            }
+        end,
     },
 }
 return config
