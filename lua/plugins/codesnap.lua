63c7a6af3618e46c5ea59dde323d4010fd318dbf
Dennis Vidovic <dennis.vidovic@meraki.net>
2024-10-15 09:47:14 -0700

    Updated codenspa

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -6,6 +6,11 @@ local config = {
             require("codesnap").setup({
                 save_path="~/codesnap",
                 has_breadcrumbs = true,
+                mac_window_bar = false,
+                bg_x_padding = 20,
+                bg_y_padding = 20,
+                bg_padding = 10,
+                watermark = "",
             })
         end,
     },
