bd90ecb67f6a2c8ddd291aa4b60db1505070be01
Dennis Vidovic <dvidovic@meraki.com>
2024-12-12 11:02:25 -0800

    Updated tmux nav with faster options

--- lua/plugins/tmux-nav.lua
+++ lua/plugins/tmux-nav.lua
@@ -6,14 +6,43 @@ local config = {
             tmux_nav.setup({
                 disable_when_zoomed = true,
             })
+
+            -- Check if running inside tmux
+            -- TODO when switching from neovim to neovim, could the exit run after the enter, resulting in TMUX_VIM
+            -- being unset still?
+            local function set_tmux_vim()
+                if os.getenv("TMUX") then
+                    os.execute("tmux setenv TMUX_VIM 1")
+                end
+            end
+
+            local function unset_tmux_vim()
+                if os.getenv("TMUX") then
+                    os.execute("tmux setenv -u TMUX_VIM")
+                end
+            end
+
+            -- TODO add these autocmds to legendary?
+            -- Set the variable when Neovim starts
+            vim.api.nvim_create_autocmd("FocusGained", {
+                callback = set_tmux_vim
+            })
+
+            -- Unset the variable when Neovim exits
+            vim.api.nvim_create_autocmd(
+                { "FocusLost", "VimLeave" },
+                {
+                    callback = unset_tmux_vim
+                })
+
             local legendary = safe_require('legendary')
             -- local tmux_nav = safe_require('nvim-tmux-navigator')
             local lsp_legend = {}
             if tmux_nav then
                 lsp_legend = {
-                    { '<C-h>', function() tmux_nav.NvimTmuxNavigateLeft() end, description = 'Tmux aware move to left window' },
-                    { '<C-j>', function() tmux_nav.NvimTmuxNavigateDown() end, description = 'Tmux aware move to down window' },
-                    { '<C-k>', function() tmux_nav.NvimTmuxNavigateUp() end,   description = 'Tmux aware move to up window' },
+                    { '<C-h>', function() tmux_nav.NvimTmuxNavigateLeft() end,  description = 'Tmux aware move to left window' },
+                    { '<C-j>', function() tmux_nav.NvimTmuxNavigateDown() end,  description = 'Tmux aware move to down window' },
+                    { '<C-k>', function() tmux_nav.NvimTmuxNavigateUp() end,    description = 'Tmux aware move to up window' },
                     { '<C-l>', function() tmux_nav.NvimTmuxNavigateRight() end, description = 'Tmux aware move to right window' },
                 }
             else
