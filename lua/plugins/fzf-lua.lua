a1cda2ff796da4f1eacce49858af33033644c91f
Dennis Vidovic <dennis.vidovic@meraki.net>
2025-02-10 11:46:55 -0800

    Updated fzf-lua binds

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -5,14 +5,24 @@ local config = {
         config = function()
             local actions = require('fzf-lua.actions')
             require("fzf-lua").setup({
-                -- keymap = {
-                --     builtin = {
-                --     },
-                -- },
+                keymap = {
+                    builtin = {
+                        ["<C-d>"] = "preview-page-down",
+                        ["<C-u>"] = "preview-page-up",
+                        ["<F1>"]  = "toggle-help",
+                        ["<F2>"]  = "toggle-fullscreen",
+                    },
+                    fzf = {
+                        ["ctrl-w"] = "toggle-all",
+                        ["ctrl-d"] = "preview-page-down",
+                        ["ctrl-u"] = "preview-page-up",
+                    }
+                },
                 defaults = {
                     formatter = "path.filename_first",
                 },
                 winopts = {
+                    fullscreen = true,
                     preview = {
                         wrap = 'wrap',
                         layout = 'vertical',
@@ -21,6 +31,15 @@ local config = {
                 },
                 oldfiles = {
                     include_current_session = true,
+                },
+                git = {
+                    status = {
+                        actions = {
+                            ["ctrl-l"] = { fn = actions.git_unstage, reload = true },
+                            ["ctrl-h"] = { fn = actions.git_stage, reload = true },
+                            ["ctrl-x"] = { fn = actions.git_reset, reload = true },
+                        },
+                    },
                 },
                 previewers = {
                     builtin = {
