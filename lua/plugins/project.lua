2e5f1adeb0221240a06400bbd7e774a18d0f93a0
Dennis Vidovic <dvidovic@meraki.com>
2025-02-10 10:58:51 -0800

    Updated project to only use files, not lsp.

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -3,8 +3,9 @@ local config = {
         'ahmedkhalf/project.nvim',
         config = function()
             safe_require("project_nvim").setup({
-                detection_methods = { 'lsp', 'pattern' },
-                patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "package.json" },
+                -- detection_methods = { 'lsp', 'pattern' },
+                detection_methods = { 'pattern' },
+                patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "package.json", "Makefile" },
                 ignore_lsp = { "lua_ls", "pyright" },
                 show_hidden = false,
                 silent_chdir = false,
